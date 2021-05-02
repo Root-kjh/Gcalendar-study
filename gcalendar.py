5c18c5b461e25e848886812e0c3510797f61a161
JONGHYUN KIM <wldnro3141@gmail.com>
2021-05-02 15:38:31 +0900

    update get event code

--- gcalendar.py
+++ gcalendar.py
@@ -1,12 +1,40 @@
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
-from google.oauth2 import id_token
-from google_auth_oauthlib.flow import InstalledAppFlow
 import time
+import jwt
+import json
 import requests
 
+def get_signed_jwt(token_json):
+    private_key_id = token_json.get("private_key_id")
+    private_key = token_json.get("private_key")
+
+    iat = time.time()
+    exp = iat + 3600
+    payload = {
+        'iss': token_json.get("client_email"),
+        'scope': 'https://www.googleapis.com/auth/calendar.readonly',
+        'aud': token_json.get("token_uri"),
+        'iat': iat,
+        'exp': exp
+    }
+    additional_headers = {'kid': private_key_id} 
+    signed_jwt = jwt.encode(payload, private_key, headers=additional_headers,algorithm='RS256') 
+    return signed_jwt
+
+def get_access_token():
+    token_json = json.load(open("env/token.json"))
+    jwt = get_signed_jwt(token_json)
+    response = requests.post(
+        token_json.get("token_uri"),
+        data = {
+            'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            'assertion': jwt
+        }
+    )
+    response.raise_for_status()
+    return response.json()['access_token']
+
 if __name__ == '__main__':
-    access_token = "AIzaSyDGRXC6hBvdf7ZsFYFhyPv9Z779ZMlgJOg"
+    access_token = get_access_token()
     calendar_id = "june.333@humanscape.io"
     curtime = time.strftime('%Y-%m-%dT00:00:00-0000', time.localtime(time.time()))
     URL = 'https://www.googleapis.com/calendar/v3/calendars/'+calendar_id +'/events'+'?timeMin='+curtime
@@ -17,4 +45,5 @@ if __name__ == '__main__':
     }
     response = requests.get(URL, headers=request_header)
 
-    print(response.text)
+    for event in response.json().get("items"):
+        print(event.get("summary"))
